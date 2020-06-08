52c9d82092c77ca20cb50bf2469450b29abcd40d
Mehmood7 <mjhassan321@gmail.com>
2020-06-08 21:30:19 +0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './stile.css'
+const App = ({friends})=> (
+    <div>
+      <h1 className="green"> Hello from <strong>Mehmood</strong></h1>
+      <hr/>
+      <h4>I have {friends-4} firends</h4>
+      <h3>My Friends</h3>
+      <ol>
+        <li>Umar</li>
+        <li>Nouman</li>
+        <li>Ali</li>
+      </ol>
     </div>
-  );
-}
+)
 
 export default App;
